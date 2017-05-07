3c6a94b072127465667d8d7d2b090975dc25d436
mlpatel2 <mahesh.patel2@thermofisher.com>
2017-05-06 20:46:43 -0400

    Add files via upload

---
+++ two.c
@@ -0,0 +1,18 @@
+//programme to test debug
+
+#include <stdio.h>
+int get_next_square(void);
+int main()
+{
+int i;
+for (i = 0; i < 10; i++)
+printf ("%6d\n", get_next_square ());
+printf ("and %6d\n", get_next_square ());
+return 0;
+}
+int get_next_square ()
+{
+static int count = 1;
+count += 1;
+return count * count;
+}
